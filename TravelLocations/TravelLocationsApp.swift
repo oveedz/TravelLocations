5a81ecee639204f583eb9e508fd9d4bf80183dde
oveedz <og17@my.fsu.edu>
2022-05-25 11:43:27 -0400

    Initial Commit

---
+++ TravelLocations/TravelLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelLocationsApp.swift
+//  TravelLocations
+//
+//  Created by Ovidio  on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
